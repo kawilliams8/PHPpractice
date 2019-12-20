5d25685eb3cc003e8f9363d9ef980229eaf5d3b6
Katie Williams <kawilliams8@gmail.com>
2019-12-20 16:35:33 -0700

    Echo the sum of num1 and num2

--- form.php
+++ form.php
@@ -7,13 +7,22 @@
   <title>Document</title>
 </head>
 <body>
-  <form action="form.php" method="get">
-    Name: <input type="text" name="name">
-    Age: <input type="number" name="age">
-    <input type="submit">
-  </form>
-  <br>
-  Your name is <?php echo $_GET["name"] ?>
-  Your age is <?php echo $_GET["age"] ?>
+  <h2>User input form</h2>
+    <form action="form.php" method="get">
+      Name: <input type="text" name="name">
+      Age: <input type="number" name="age">
+      <input type="submit">
+    </form>
+    <br>
+    Your name is <?php echo $_GET["name"] ?>
+    Your age is <?php echo $_GET["age"] ?>
+  <h2>Calculator</h2>
+    <form action="form.php" method="get">
+      Number 1: <input type="number" name="num1">
+      <br>
+      Number 2: <input type="number" name="num2">
+      <input type="submit">
+    </form>
+  Total: <?php echo $GET["num1"] + $_GET["num2"] ?>
 </body>
 </html>
