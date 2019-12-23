3d5bf45136a4144d1f39c34fccf4dea192eeca5f
Katie Williams <kawilliams8@gmail.com>
2019-12-23 16:32:39 -0700

    Finish getters and setters

--- getters-and-setters/site.php
+++ getters-and-setters/site.php
@@ -9,18 +9,31 @@
 <body>
   <?php
     class Movie {
-      public $title;
-      public $rating;
+      private $title; //This attr is visible and modifiable anywhere else (global)
+      public $rating; //var and public would be synonymous here
 
       function __construct($title, $rating) {
         $this->title = $title;
-        $this->rating = $rating;
+        $this->rating = $rating; //Could also call a setRating method on a private attr
+        //$this->setRating($rating);
+      }
+
+      function getTitle(){
+        return "<h2>$this->title</h2>";
+      }
+
+      function setTitle($title){
+        $this->title = $title;
       }
     }
 
     $avengers = new Movie("Avengers", "PG-13");
-    echo $avengers->title;
+    // echo $avengers->title; //Triggers error, due to private keyword
     echo $avengers->rating; 
+
+    echo $avengers->getTitle();
+    echo $avengers->setTitle("Another Movie");
+    echo $avengers->getTitle();
   ?>
 </body>
 </html>
