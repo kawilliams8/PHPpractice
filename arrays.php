45be4993085651244021e061d9a9c414b51d05c2
Katie Williams <kawilliams8@gmail.com>
2019-12-23 10:42:19 -0700

    Finish array display

--- arrays.php
+++ arrays.php
@@ -4,11 +4,18 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Arrays</title>
 </head>
 <body>
   <?php
     $friends = array("Kevin", "Karen", "Oscar", "Jim");
+    $friends[1] = "Dwight"; //Reassignment
+    $friends[4] = 400; //Add new index 
+    echo $friends[0];
+  ?>
+  <br>
+  <?php
+    echo count($friends); //Access globally scoped variable?
   ?>
 </body>
 </html>
