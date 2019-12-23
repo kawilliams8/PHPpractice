5dd85d0e4957d8f3506cd62506b1f8ed918e5743
Katie Williams <kawilliams8@gmail.com>
2019-12-23 11:33:48 -0700

    Touch return.php

--- return.php
+++ return.php
@@ -4,16 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Functions</title>
+  <title>Return statements</title>
 </head>
 <body>
-  <?php
-    function sayHi($name, $age){
-      echo "Hi $name, you are $age years old. <br>";
-    }
-
-    sayHi("Katie", 21);
-    sayHi("Someone else", 80);
-  ?>
+  
 </body>
 </html>
