3f1bfee27f9cd5b04d5cfce99eb13988a4480277
Katie Williams <kawilliams8@gmail.com>
2019-12-23 15:29:40 -0700

    Finish constructor function update

--- classes-and-objects/site.php
+++ classes-and-objects/site.php
@@ -12,25 +12,31 @@
       var $title;
       var $author;
       var $numPages;
+      var $other;
+
+      function __construct($title, $author, $numPages, $other = 'hi'){ //Executed by the 'new' keyword
+        $this->title = $title;
+        $this->author = $author;
+        $this->numPages = $numPages;
+        $this->other = $other; //Passing default value
+      }
     }
 
-    $book1 = new Book;
-    $book1->title = "Harry Potter";
-    $book1->author = "JK Rowling";
-    $book1->numPages = 400;
+    $book1 = new Book("Harry Potter", "JK Rowling", 400);
 
     echo "<h1>$book1->title</h1>";
     echo "<h2>$book1->author</h2>";
     echo "<h2>$book1->numPages</h2>";
+    echo "<h2>$book1->other</h2>";
 
-    $book2 = new Book;
-    $book2->title = "Lord of the Rings";
-    $book2->author = "Tolkein";
-    $book2->numPages = 700;
+    // $book2 = new Book;
+    // $book2->title = "Lord of the Rings";
+    // $book2->author = "Tolkein";
+    // $book2->numPages = 700;
 
-    echo "<h1>$book2->title</h1>";
-    echo "<h2>$book2->author</h2>";
-    echo "<h2>$book2->numPages</h2>";
+    // echo "<h1>$book2->title</h1>";
+    // echo "<h2>$book2->author</h2>";
+    // echo "<h2>$book2->numPages</h2>";
   ?>
 </body>
 </html>
