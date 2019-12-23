6cc484fb7ba6c34b44d7738a5656e08ca51abb8e
Katie Williams <kawilliams8@gmail.com>
2019-12-23 16:36:13 -0700

    Add inheritance dir and site.php

---
+++ Inheritance/site.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Inheritance</title>
+</head>
+<body>
+  <?php
+    class Chef {
+      function makeChicken(){
+        echo "The chef makes chicken. <br>"
+      }
+
+      function makeSalad(){
+        echo "The chef makes salad. <br>"
+      }
+
+      function makeSpecialDish(){
+        echo "The chef makes the special dish. <br>"
+      }
+    }
+  ?>
+</body>
+</html>
