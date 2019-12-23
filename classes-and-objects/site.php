4d56f5ba76a30a6aceaa9019108aebf22d9299c8
Katie Williams <kawilliams8@gmail.com>
2019-12-23 15:18:52 -0700

    Add two book instances

--- classes-and-objects/site.php
+++ classes-and-objects/site.php
@@ -8,7 +8,29 @@
 </head>
 <body>
   <?php
-    
+    class Book {
+      var $title;
+      var $author;
+      var $numPages;
+    }
+
+    $book1 = new Book;
+    $book1->title = "Harry Potter";
+    $book1->author = "JK Rowling";
+    $book1->numPages = 400;
+
+    echo "<h1>$book1->title</h1>";
+    echo "<h2>$book1->author</h2>";
+    echo "<h2>$book1->numPages</h2>";
+
+    $book2 = new Book;
+    $book2->title = "Lord of the Rings";
+    $book2->author = "Tolkein";
+    $book2->numPages = 700;
+
+    echo "<h1>$book2->title</h1>";
+    echo "<h2>$book2->author</h2>";
+    echo "<h2>$book2->numPages</h2>";
   ?>
 </body>
 </html>
