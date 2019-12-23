3a087540a9beccdb5937996a8725ac3dad1695e8
Katie Williams <kawilliams8@gmail.com>
2019-12-23 10:17:16 -0700

    Touch login.php and start login form w/POST methods

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Fake Login</title>
+</head>
+<body>
+  <form action="login.php" method="post">
+    Password: <input type="password" name="password">
+    <input type="submit"
+  </form>
+</body>
+</html>
